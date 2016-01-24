7ad6d076eb14835f6461959a778bd673043176b3
Deathhush <justinsyc@gmail.com>
2016-01-24 20:51:48 +0800

    Add year range to load_df_by_year.

--- StockEvaluation/StockDataAnalyzer.py
+++ StockEvaluation/StockDataAnalyzer.py
@@ -22,10 +22,12 @@ class StockDataAnalyzer(object):
         KdjIndicator().populate(df)        
         return df         
     
-    def load_daily_df_by_year(self, symbol, year=2015):
-        df = self.fetcher.load_daily_df_by_year(symbol, year)
-        start = datetime(int(year), 1, 1).strftime('%Y/%m/%d')
-        end = datetime(int(year), 12, 31).strftime('%Y/%m/%d')
+    def load_daily_df_by_year(self, symbol, start_year=2015, end_year=None):
+        if end_year == None:
+            end_year = start_year
+        df = self.fetcher.load_daily_df_by_year(symbol, start_year)
+        start = datetime(int(start_year), 1, 1).strftime('%Y/%m/%d')
+        end = datetime(int(end_year), 12, 31).strftime('%Y/%m/%d')
 
         return self.load_daily_df(symbol, start, end)
 
